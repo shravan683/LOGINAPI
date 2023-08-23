182450d8ef909c112cfba6be8bfe8921550680d1
shravan683 <141900397+shravan683@users.noreply.github.com>
2023-08-23 09:47:29 +0530

    Add files via upload

---
+++ src/first/srializers.py
@@ -0,0 +1,14 @@
+# from rest_framework import serializers
+# from django.contrib.auth import render
+
+# class Userserializer(serializers.ModelSerializer)
+#     class Meta:
+#         model = get_user_model()
+#         fields='__all__'
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = '__all__'
